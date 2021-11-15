2a46c751bec69fd73353d4ff92c666e4f3b41007
UndefinedOffset <echipman@webbuildersgroup.com>
2021-11-15 11:28:41 -0400

    PSR Conversion
    Fixed issue where the config was not loading properly

--- src/Extensions/CMSPreviewPreference.php
+++ src/Extensions/CMSPreviewPreference.php
@@ -2,23 +2,27 @@
 namespace WebbuildersGroup\CMSPreviewPreference\Extensions;
 
 use SilverStripe\Core\Config\Config;
-use SilverStripe\Security\Member;
-use SilverStripe\View\Requirements;
 use SilverStripe\Core\Extension;
+use SilverStripe\Security\Security;
+use SilverStripe\View\Requirements;
 
-class CMSPreviewPreference extends Extension {
-    public function init() {
-        $mode=Config::inst()->get('UserPreviewPreference', 'DefaultMode');
-        $userMode=Member::currentUser()->DefaultPreviewMode;
-        
-        if(!empty($userMode)) {
-            $mode=$userMode;
+class CMSPreviewPreference extends Extension
+{
+    public function init()
+    {
+        $mode = Config::inst()->get(UserPreviewPreference::class, 'DefaultMode');
+        $userMode = Security::getCurrentUser()->DefaultPreviewMode;
+        if (!empty($userMode)) {
+            $mode = $userMode;
         }
-        
-        
-        Requirements::javascriptTemplate('webbuilders-group/silverstripe-cmspreviewpreference: javascript/preview-mode.template.js', array(
-                                                                                                                                            'PreviewMode'=>$mode
-                                                                                                                                        ), 'cms-preview-mode');
+
+
+        Requirements::javascriptTemplate(
+            'webbuilders-group/silverstripe-cmspreviewpreference: javascript/preview-mode.template.js',
+            [
+                'PreviewMode' => $mode,
+            ],
+            'cms-preview-mode'
+        );
     }
 }
-?>
--- src/Extensions/UserPreviewPreference.php
+++ src/Extensions/UserPreviewPreference.php
@@ -9,56 +9,64 @@ use SilverStripe\Control\Session;
 use SilverStripe\View\Requirements;
 use SilverStripe\ORM\DataExtension;
 
-class UserPreviewPreference extends DataExtension {
-    private static $db=array(
-                            'DefaultPreviewMode'=>"Enum(array('content', 'split', 'preview'), 'content')"
-                        );
-    
-    private static $defaults=array(
-                                'DefaultPreviewMode'=>'content'
-                            );
-    
-    private $isSaving=false;
-    
-    
+class UserPreviewPreference extends DataExtension
+{
+    private static $db = [
+        'DefaultPreviewMode' => "Enum(array('content', 'split', 'preview'), 'content')",
+    ];
+
+    private static $defaults = [
+        'DefaultPreviewMode' => 'content',
+    ];
+
+    private $isSaving = false;
+
+
     /**
      * Updates the fields used in the cms
      * @param {FieldList} $fields Fields to be extended
      */
-    public function updateCMSFields(FieldList $fields) {
-        
-        $fields->addFieldToTab('Root.Main', $field=new OptionsetField('DefaultPreviewMode', _t('UserPreviewPreference.DEFAULT_MODE', '_Default Preview Mode'), array(
-                                                                                                                                                            'content'=>_t('UserPreviewPreference.CONTENT_MODE', '_Content Mode: Only menu and content areas are shown'),
-                                                                                                                                                            'split'=>_t('UserPreviewPreference.SPLIT_MODE', '_Split Mode: Side by Side editing and previewing'),
-                                                                                                                                                            'preview'=>_t('UserPreviewPreference.PREVIEW_MODE', '_Preview Mode: Only menu and preview areas are shown')
-                                                                                                                                                        ), Config::inst()->get('UserPreviewPreference', 'DefaultMode')));
-        
-        if(Controller::curr()->getRequest()->getSession()->get('ShowPreviewSettingChangeReload')==true) {
-            $field->setMessage(_t('UserPreviewPreference.CHANGE_REFRESH', '_You have updated your preview preference, you must refresh your browser to see the updated setting'), 'warning');
+    public function updateCMSFields(FieldList $fields)
+    {
+        $fields->addFieldToTab(
+            'Root.Main',
+            $field = new OptionsetField(
+                'DefaultPreviewMode',
+                _t(__CLASS__ . '.DEFAULT_MODE', '_Default Preview Mode'),
+                [
+                    'content' => _t(__CLASS__ . '.CONTENT_MODE', '_Content Mode: Only menu and content areas are shown'),
+                    'split' => _t(__CLASS__ . '.SPLIT_MODE', '_Split Mode: Side by Side editing and previewing'),
+                    'preview' => _t(__CLASS__ . '.PREVIEW_MODE', '_Preview Mode: Only menu and preview areas are shown'),
+                ],
+                Config::inst()->get(UserPreviewPreference::class, 'DefaultMode')
+            )
+        );
+
+        if (Controller::curr()->getRequest()->getSession()->get('ShowPreviewSettingChangeReload') == true) {
+            $field->setMessage(_t(__CLASS__ . '.CHANGE_REFRESH', '_You have updated your preview preference, you must refresh your browser to see the updated setting'), 'warning');
             Requirements::javascript('webbuilders-group/silverstripe-cmspreviewpreference: javascript/clear-local-preference.js');
-            
-            if($this->isSaving==false) {
+
+            if ($this->isSaving == false) {
                 Controller::curr()->getRequest()->getSession()->clear('ShowPreviewSettingChangeReload');
             }
         }
     }
-    
+
     /**
      * Ensures config defaults are enforced on write
-     */ 
-    public function onBeforeWrite() {
+     */
+    public function onBeforeWrite()
+    {
         parent::onBeforeWrite();
-        
-        if(empty($this->owner->DefaultPreviewMode)) {
-            $this->owner->DefaultPreviewMode=Config::inst()->get('UserPreviewPreference', 'DefaultMode');
+
+        if (empty($this->owner->DefaultPreviewMode)) {
+            $this->owner->DefaultPreviewMode = Config::inst()->get(UserPreviewPreference::class, 'DefaultMode');
         }
-        
-        
+
         //If changed ensure their is a session message
-        if($this->owner->isChanged('DefaultPreviewMode')) {
+        if ($this->owner->isChanged('DefaultPreviewMode')) {
             Controller::curr()->getRequest()->getSession()->set('ShowPreviewSettingChangeReload', true);
-            $this->isSaving=true;
+            $this->isSaving = true;
         }
     }
 }
-?>
